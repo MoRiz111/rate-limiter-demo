dfb28c3967f3287b4a7e03df677cacc2f1b07e39
Riyas <mohammedriyas.mohamedyasin@ideas2it.com>
2026-02-27 16:42:02 +0530

    Update : Spring project boiler plate

---
+++ src/main/java/com/example/ratelimiterdemo/RatelimiterdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimiterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimiterdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatelimiterdemoApplication.class, args);
+	}
+
+}
