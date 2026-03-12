b7e289bbeae18c42031752f235b507195f44dc8c
Riyas <mohammedriyas.mohamedyasin@ideas2it.com>
2026-03-12 17:10:43 +0530

    Update : Rate Limiter v2 - made thread safe and concurrency handled

--- src/main/java/com/example/ratelimiterdemo/service/RateLimiterService.java
+++ src/main/java/com/example/ratelimiterdemo/service/RateLimiterService.java
@@ -1,34 +1,37 @@
 package com.example.ratelimiterdemo.service;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class RateLimiterService {
-    private final Map<String, Queue<Long>> requestMap = new HashMap<>();
+    private final Map<String, Queue<Long>> requestMap = new ConcurrentHashMap<>();
 
     private static final int REQUEST_LIMIT = 5;
     private static final long TIME_WINDOW = 10000; // 10 seconds
 
     public boolean allowRequest(final String userId) {
         final long currentTime = System.currentTimeMillis();
-        requestMap.putIfAbsent(userId, new LinkedList<Long>());
+        requestMap.putIfAbsent(userId, new ConcurrentLinkedDeque<>());
         Queue<Long> timeStamps = requestMap.get(userId);
 
-        // remove expired timestamps
-        while (!timeStamps.isEmpty() && currentTime - timeStamps.peek() > TIME_WINDOW) {
-            timeStamps.poll();
-        }
+        synchronized (timeStamps) {
+            // critical block
+            // remove expired timestamps
+            while (!timeStamps.isEmpty() && currentTime - timeStamps.peek() > TIME_WINDOW) {
+                timeStamps.poll();
+            }
 
-        if (timeStamps.size() >= REQUEST_LIMIT) {
-            return false;
-        }
+            if (timeStamps.size() >= REQUEST_LIMIT) {
+                return false;
+            }
 
-        timeStamps.add(currentTime);
-        return true;
+            timeStamps.add(currentTime);
+            return true;
+        }
     }
 }
